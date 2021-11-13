9cd236fd329edf7c54d3f7ddebb9c47401a5df38
Viktor Moroz <moroz@newtonideas.com>
2021-11-13 17:56:53 +0200

    translations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,20 @@ import { HomeComponent } from './routes/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NoSanitizePipe } from './pipes/no-sanitize';
 import { ProjectDetailsComponent } from './routes/project-details/project-details.component';
 import { ProjectsListComponent } from './routes/projects-list/projects-list.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+const defaultLang = 'en';
+const langs = ['en', 'de'];
+const useLang = langs.find(x => navigator.language.indexOf(x) > 0) ? navigator.language : defaultLang;
 
 @NgModule({
   declarations: [
@@ -26,7 +36,15 @@ import { ProjectsListComponent } from './routes/projects-list/projects-list.comp
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: useLang,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { take, tap } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,37 @@ export class DataService {
   private _data: BehaviorSubject<any> = new BehaviorSubject<any>(undefined);
   data$ = this._data.asObservable();
 
-  constructor(private http: HttpClient) {
+  defaultLang = 'en';
+  langs = ['en', 'de'];
+  useLang = this.langs.find(x => navigator.language.indexOf(x) > 0)
+    ? navigator.language : this.defaultLang;
+  language$: BehaviorSubject<string> = new BehaviorSubject<string>(this.useLang);
+
+  constructor(private http: HttpClient, private translateService: TranslateService) {
   }
 
   fetchData(): Observable<any> {
-    return this.http.get('/assets/data/data.json?cb=' + new Date().getTime()).pipe(
+    return this.http.get<any>('/assets/data/data.json?cb=' + new Date().getTime()).pipe(
       take(1),
+      map(r => {
+        const lang = this.language$.getValue();
+        Object.keys(r).forEach(key => {
+          const value = r[key];
+          if (value[lang]) {
+            Object.keys(value[lang]).forEach(langKey => {
+              value[langKey] = value[lang][langKey];
+            })
+          }
+        })
+        return r;
+      }),
       tap(r => {
         this._data.next(r);
       })
     )
+  }
+
+  getLang(): string {
+    return this.language$.getValue();
   }
 }
