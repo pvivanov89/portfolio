05e90d4ee1050fbd35cf3186019d99171675b87a
Viktor Moroz <moroz@newtonideas.com>
2021-11-14 16:36:40 +0200

    add pdf preview

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { ViewSDKClient } from '../../services/view-sdk.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor(public dataService: DataService) { }
+  constructor(public dataService: DataService, private viewSDKClient: ViewSDKClient) { }
 
   ngOnInit(): void {
 
   }
 
+  ngAfterViewInit() {
+    this.viewSDKClient.ready().then(() => {
+      /* Invoke file preview */
+      this.viewSDKClient.previewFile('pdf-div', {
+        /* Pass the embed mode option here */
+        embedMode: 'SIZED_CONTAINER'
+      });
+    });
+  }
 }
---
+++ src/app/services/view-sdk.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ViewSDKClient {
+
+  readyPromise: Promise<any> = new Promise((resolve) => {
+    // @ts-ignore
+    if (window.AdobeDC) {
+      // @ts-ignore
+      resolve();
+    } else {
+      /* Wait for Adobe Document Services PDF Embed API to be ready */
+      document.addEventListener('adobe_dc_view_sdk.ready', () => {
+        // @ts-ignore
+        resolve();
+      });
+    }
+  });
+  adobeDCView: any;
+
+  ready() {
+    return this.readyPromise;
+  }
+
+  previewFile(divId: string, viewerConfig: any) {
+    const config: any = {
+      /* Pass your registered client id */
+      clientId: '3ee5a59b2f984319848405c565909544',
+    };
+    if (divId) { /* Optional only for Light Box embed mode */
+      /* Pass the div id in which PDF should be rendered */
+      config.divId = divId;
+    }
+    /* Initialize the AdobeDC View object */
+    // @ts-ignore
+    this.adobeDCView = new window.AdobeDC.View(config);
+
+    /* Invoke the file preview API on Adobe DC View object */
+    const previewFilePromise = this.adobeDCView.previewFile({
+      /* Pass information on how to access the file */
+      content: {
+        /* Location of file where it is hosted */
+        location: {
+          url: '/assets/cv_de.pdf',
+          /*
+          If the file URL requires some additional headers, then it can be passed as follows:-
+          headers: [
+              {
+                  key: '<HEADER_KEY>',
+                  value: '<HEADER_VALUE>',
+              }
+          ]
+          */
+        },
+      },
+      /* Pass meta data of file */
+      metaData: {
+        /* file name */
+        fileName: 'cv_de.pdf',
+        /* file ID */
+        id: '6d07d124-ac85-43b3-a867-36930f502ac6',
+      }
+    }, viewerConfig);
+
+    return previewFilePromise;
+  }
+
+}
