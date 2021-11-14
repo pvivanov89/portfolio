e76bd8ec03af911f929fe4d1213269681960a0d7
Viktor Moroz <moroz@newtonideas.com>
2021-11-14 17:27:51 +0200

    list

--- src/app/routes/projects-list/projects-list.component.ts
+++ src/app/routes/projects-list/projects-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-projects-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dataService: DataService) { }
 
   ngOnInit(): void {
   }
